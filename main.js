3a59854446fe622709db7957359c4d940515a4ca
Will Sims <will@execad.com>
2017-09-15 16:27:05 -0500

    Added restaurants

--- main.js
+++ main.js
@@ -17,23 +17,20 @@ $("#shuffle-btn").on('click', function(event) {
 
         var random_rest_obj = restaurants[~~(Math.random() * restaurants.length)]
 
-        console.log("MONEY: ", random_rest_obj.money);
-
         var booze_string = "No Booze";
         var money_string = random_rest_obj.money;
         var menu_link_string_1 = random_rest_obj.menu_img;
         var menu_link_string_2 = random_rest_obj.menu_img_2;
-
-
-
 
         if (random_rest_obj.has_booze){
             booze_string = "Has Booze";
         }
 
         var random_rest_string =    `<h2 class="lead">${random_rest_obj.name}</h2>
-                                     <p class="flow-text">${money_string}</p>
-                                     <p class="flow-text">${booze_string}</p>`;
+                                    <p class="flow-text">${random_rest_obj.type}</p>
+                                    <p class="flow-text">${booze_string}</p>
+                                    <p class="flow-text">${money_string}</p>`;
+
 
         if (random_rest_obj.pat_approved == true){
             random_rest_string +=  `<p class="flow-text"><strong>BONUS: </strong>Pat Approved!</p>`;
@@ -41,20 +38,21 @@ $("#shuffle-btn").on('click', function(event) {
 
         if (random_rest_obj.menu_url != ""){
             random_rest_string += `<a href="${random_rest_obj.menu_url}">View Menu</a>`;
+            if (random_rest_obj.menu_url_2 != ""){
+                random_rest_string += `<br><a href="${random_rest_obj.menu_url_2}">View Other Menu</a>`;
+            }
         }
 
 
         if(menu_link_string_1 != "" && menu_link_string_2 != ""){
             random_rest_string += `  <div class="row center">
-                                    <img class="col s6" src="${menu_link_string_1}"></a>
-                                    <img class="col s6" src="${menu_link_string_2}"></a>
+                                    <img class="my_image col s6" src="${menu_link_string_1}"></a>
+                                    <img class="my_image col s6" src="${menu_link_string_2}"></a>
                                     </div>`;
 
         }else if(menu_link_string_1 != ""){
-            random_rest_string += `<img class="col s6" src="${menu_link_string_1}"></a>`;
+            random_rest_string += `<img class="my_image col s6" src="${menu_link_string_1}"></a>`;
         }
-
-
 
         $("#random_rest_container").html(random_rest_string);
 
